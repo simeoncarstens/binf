cb2550c5e8f9c77a7dad3456558b7eafb0c9fb6f
simeon_carstens <simeon_carstens@localhost>
2015-01-29 14:51:46 +0100

    Refactored Posterior and Likelihood classes
    They now share some methods commo to both of them because they are "composed" functions, but atm these methods are imported from a module. I need to derive both classes from some ComposedISDCallable (or similar) class

--- isd2/pdf/posteriors.py
+++ isd2/pdf/posteriors.py
@@ -9,6 +9,7 @@ import numpy
 from csb.numeric import log, exp
 
 from isd2 import AbstractISDNamedCallable
+from isd2.core.composed import _get_component_var_param_types, _setup_variables, _setup_parameters, fix_variables
 from isd2.pdf import AbstractISDPDF
 
 
@@ -21,53 +22,23 @@ class Posterior(AbstractISDPDF):
         self._likelihoods = likelihoods
         self._priors = priors
 
-        self._setup_parameters()
         self._components = dict(**self.priors)
         self._components.update(**self.likelihoods)
-        self._register_component_variables(*self._get_component_variables())
+        
+        self._setup_variables()
+        self.update_var_param_types(**self._get_component_var_param_types())
+        self._setup_parameters()
 
         self._set_original_variables()
 
-    def _setup_parameters(self):
+    _get_component_var_param_types = _get_component_var_param_types
 
-        for L in self.likelihoods.values():
-            for p in L.parameters:
-                if p not in self.parameters:
-                    self._register(p)
-                    self[p] = L[p].__class__(L[p].value, 
-                                             L[p].name)
-                L[p].bind_to(self[p])
+    _setup_variables = _setup_variables
 
-        for P in self.priors.values():
-            for p in P.parameters:
-                if p not in self.parameters:
-                    self._register(p)
-                    self[p] = P[p].__class__(P[p].value, 
-                                             P[p].name)
-                P[p].bind_to(self[p])
+    _setup_parameters = _setup_parameters
+
+    fix_variables = fix_variables
     
-    def _get_component_variables(self):
-
-        vars = []
-        diff_vars = []
-        var_param_types = []
-
-        for c in self._components:
-            for v in self._components[c].variables:
-                vars.append(v)
-                var_param_types.append(self._components[c].var_param_types[v])
-                if v in self._components[c].differentiable_variables:
-                    diff_vars.append(v)
-
-        return set(vars), set(diff_vars), var_param_types
-
-    def _register_component_variables(self, vars, diff_vars, var_param_types):
-
-        for var in vars:
-            self._register_variable(str(var), differentiable=var in diff_vars)
-
-        self.update_var_param_types(**{var: _type for var, _type in zip(vars, var_param_types)})
-
     def _get_component_variables_list(self):
 
         return {c: c.variables for c in self._components.values()}
@@ -101,14 +72,12 @@ class Posterior(AbstractISDPDF):
 
     def _evaluate_gradient(self, **variables):
 
-        vars = variables
-
         res = numpy.zeros(sum([len(variables[v]) for v in variables 
                                if v in self.differentiable_variables]))
 
         for n, f in self._components.iteritems():
             if len(f.variables) > 0 and len(f.differentiable_variables) > 0:
-                res += f.gradient(**{x: vars[x] for x in vars 
+                res += f.gradient(**{x: variables[x] for x in variables 
                                      if x in f.variables})
 
         return res
@@ -119,18 +88,5 @@ class Posterior(AbstractISDPDF):
                               {P: self.priors[P].clone() for P in self.priors}, 
                               self.name)
 
-        copy.set_fixed_variables_from_pdf(self)
-        
         return copy
 
-    def conditional_factory(self, **fixed_vars):
-
-        conditional_likelihoods = {L: self.likelihoods[L].conditional_factory(**fixed_vars) 
-                                   for L in self.likelihoods}
-        conditional_priors = {P: self.priors[P].conditional_factory(**fixed_vars) 
-                              for P in self.priors}
-
-        copy = self.__class__(conditional_likelihoods, conditional_priors, self.name)
-        
-        return copy
-        
