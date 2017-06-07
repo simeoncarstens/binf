0cb7031a20b8c4509a25f2bd3070d2409d42d77b
Simeon Carstens <simeon.carstens@mpibpc.mpg.de>
2017-06-07 17:58:36 +0200

    Extended ISD2 PDFWrapper to accommodate more than one variable

--- isd2/samplers/pdfwrapper.py
+++ isd2/samplers/pdfwrapper.py
@@ -8,15 +8,17 @@ class PDFWrapper(AbstractDensity):
     def __init__(self, isd2pdf):
 
         self.isd2pdf = isd2pdf
-        self.vn = list(self.isd2pdf.variables)[0]
+        self.vns = list(self.isd2pdf.variables)
 
     def log_prob(self, x):
 
-        return self.isd2pdf.log_prob(**{self.vn: x})
+        return self.isd2pdf.log_prob(**{vn: x.variables[vn]
+                                        for vn in self.vns})
 
     def gradient(self, x, t=0.0):
 
-        return self.isd2pdf.gradient(**{self.vn: x})
+        return self.isd2pdf.gradient(**{vn: x.variables[vn]
+                                        for vn in self.vns})
 
     @property
     def variables(self):
