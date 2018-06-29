87a5ad1f14a0961a169f698bf4b169e791146b35
Simeon Carstens <simeon.carstens@mpibpc.mpg.de>
2018-06-29 19:04:38 +0200

    Fixed start directory for tests

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
 loader = unittest.TestLoader()
-tests = loader.discover('./tests/', pattern='*.py')
+tests = loader.discover('./binf/tests/', pattern='*.py')
 testRunner = unittest.runner.TextTestRunner()
 testRunner.run(tests)
